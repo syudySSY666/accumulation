e6e6f29db1cfdcaaf5911e9e8f459f840af6a09d
方官红 <fangguanhong@netrain.com>
2023-02-09 21:49:17 +0800

    项目基础结构建立

---
+++ src/main/java/test.java
@@ -0,0 +1,18 @@
+/**
+ * @author fangguanhong
+ * @since 2023/2/9
+ */
+
+/**
+ * test
+ *
+ * @version
+ * @author fangguanhong
+ * @date 2023/2/9 9:44 下午
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
