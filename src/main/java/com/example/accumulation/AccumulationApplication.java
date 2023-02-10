281653f79dd3aaf44abf7d8257a2710b02b9b818
方官红 <fangguanhong@netrain.com>
2023-02-10 20:59:22 +0800

    初始化spring boot项目

---
+++ src/main/java/com/example/accumulation/AccumulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.accumulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccumulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccumulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accumulation/AccumulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accumulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccumulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
